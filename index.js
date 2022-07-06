3eaccbdf434d6412d0bd5a3cc84a8c6f0e7b4466
hogehogeBrav <zmzmakoto@icloud.com>
2022-07-06 23:19:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var exp = require("express");
+var app = exp();
+var config = require("./config/host");
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: config.dbHost,
+  port: config.dbPort,
+  user: config.dbUser,
+  password: config.dbPassword,
+  database: config.dbDatabase
+});
+
+app.get("/insects", function (req, res) {
+  connection.query('select * from insects', function (error, results, fields) {
+      if (error) throw error;
+      res.send(results);
+  });
+});
+
+app.get("/insects/:insects_id", function (req, res) {
+  connection.query('select * from insects where insect_id = ' + req.params.insects_id, function (error, results, fields) {
+      if (error) throw error; 
+      res.send(results);
+  });
+});
+
+app.listen(3000,function(){
+    console.log("成功")
+})
